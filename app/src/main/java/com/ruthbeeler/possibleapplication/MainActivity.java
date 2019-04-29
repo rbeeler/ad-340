2ebd93f565ab5aaa400b05aa4b3c35cef1ef5e4a
rbeeler <ruthbeeler3@gmail.com>
2019-04-29 16:15:45 -0700

    trying commit

---
+++ app/src/main/java/com/ruthbeeler/possibleapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.ruthbeeler.possibleapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayToast(View view) {
+        Toast.makeText(getApplicationContext(),"Hello World",Toast.LENGTH_SHORT).show();
+    }
+
+    public void getMovieList(View view) {
+        Intent intent = new Intent(this, listMoviesActivity.class);
+        startActivity(intent);
+    }
+}
