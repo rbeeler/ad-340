07811b882265e70426c49dcb97227e299ae2e8d3
Laila Chavez <lailamay.chavez@gmail.com>
2019-05-11 13:56:25 -0700

    creating a menu icon in toolbar with a drop down

--- app/src/main/java/com/ruthbeeler/possibleapplication/MainActivity.java
+++ app/src/main/java/com/ruthbeeler/possibleapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.ToolbarWidgetWrapper;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Toast;
 import android.content.Intent;
@@ -19,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+
 
     public void displayToast(View view) {
         Toast.makeText(getApplicationContext(),"Hello World",Toast.LENGTH_SHORT).show();
